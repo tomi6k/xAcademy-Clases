4660f088f90e79eed48cb6b9369d8c9f5f7d8788
tomi6k <tms1csgo@gmail.com>
2023-05-11 11:17:29 -0300

    subo tarea 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# En este repositorio guardare el avance de clases de xAcademy 2023 - Santex 
--- Tarea JS1/punto1/app.js
+++
@@ -1,9 +0,0 @@
-function piramide(numero) {
-  for (let i = 1; i <= numero; i++) {
-    let linea = '';
-    for (let j = 1; j <= i; j++) {
-      linea += j;
-    }
-    console.log(linea);
-  }
-}
